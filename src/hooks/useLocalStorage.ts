ff6f08331e0be76d8ae29b7375b3f9631bcf7f27
PablCendrero8310 <>
2024-04-06 16:01:31 +0200

    "Create useLocalStorage hook"

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,3 +1,21 @@
-function useLocalStorage<T>() {
-    
+import { useState } from "react";
+export default function useLocalStorage<T = string>(key: string, initialValue: T): any[] {
+    const [storedValue, setStoredValue] = useState<string>(() => {
+        try {
+            const item = window.localStorage.getItem(key)
+            return item ? JSON.parse(item) : initialValue
+        } catch (error) {
+            return initialValue
+        }
+    })
+    const setValue = (value: T) => {
+        try {
+            setStoredValue(JSON.stringify(value))
+            window.localStorage.setItem(key, storedValue)
+        } catch (error) {
+            console.error(error)
+        }
+
+    }
+    return [storedValue, setValue]
 }
